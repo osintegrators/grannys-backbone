f51e06b0a37cb0006a4f5b074707a13bb95277b3
Jonathan Freeman <freethejazz@gmail.com>
2014-03-04 17:28:43 -0600

    Adding the kick-off test script

---
+++ test/test-main.js
@@ -0,0 +1,32 @@
+var tests = [];
+for (var file in window.__karma__.files) {
+  if (window.__karma__.files.hasOwnProperty(file)) {
+    if (/Spec\.js$/.test(file)) {
+      tests.push(file);
+    }
+  }
+}
+
+requirejs.config({
+  // Karma serves files from '/base'
+  baseUrl: '/base',
+
+  paths: {
+    'jquery': 'bower_components/jquery/dist/jquery',
+    'underscore': 'bower_components/underscore/underscore',
+    'backbone': 'bower_components/backbone/backbone',
+    'handlebars': 'bower_components/handlebars/handlebars.amd'
+  },
+
+  shim: {
+    'underscore': {
+      exports: '_'
+    }
+  },
+
+  // ask Require.js to load these files (all our tests)
+  deps: tests,
+
+  // start test run, once Require.js is done
+  callback: window.__karma__.start
+});
