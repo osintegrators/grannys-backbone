6fec0fe5060aef0c71a6be9e06737fac14a5b7a2
Jonathan Freeman <freethejazz@gmail.com>
2014-03-04 17:32:55 -0600

    Adding defaults to model

--- js/models/address.js
+++ js/models/address.js
@@ -1,6 +1,10 @@
 define(['underscore', 'backbone'], function(_, Backbone){
 
   var AddressModel = Backbone.Model.extend({
+    defaults: {
+      firstName: 'Jane',
+      lastName: 'Doe'
+    }
   });
 
   return AddressModel;
