67440acb1fffcdf50b212a307d842651e501390b
Jonathan Freeman <freethejazz@gmail.com>
2014-03-05 16:48:20 -0600

    Adding address collection test

---
+++ test/collections/addressesSpec.js
@@ -0,0 +1,10 @@
+define(['js/collections/addresses'], function(AddressCollection){
+  
+  describe('The address collection', function(){
+  
+    it('should exist', function(){
+      expect(AddressCollection).toBeDefined();
+    });
+
+  });
+});
