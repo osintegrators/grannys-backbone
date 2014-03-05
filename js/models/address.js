17700f0302fa34edd92422df02a5a4cdd2f05959
Jonathan Freeman <freethejazz@gmail.com>
2014-03-04 18:12:32 -0600

    Adding validations before tests

--- js/models/address.js
+++ js/models/address.js
@@ -1,9 +1,17 @@
-define(['underscore', 'backbone'], function(_, Backbone){
+define(['underscore', 'backbone'], function(_, Backbone) {
 
   var AddressModel = Backbone.Model.extend({
     defaults: {
       firstName: 'Jane',
       lastName: 'Doe'
+    },
+    validate: function(attrs) {
+      if(!attrs.firstName){
+        return "First Name Required";
+      }
+      if(!attrs.lastName){
+        return "Last Name Required";
+      }
     }
   });
 
