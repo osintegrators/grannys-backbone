0c1e76932656ade66f72f519f84a42016e562d10
Jonathan Freeman <freethejazz@gmail.com>
2014-03-04 18:13:21 -0600

    Adding first name test

--- test/models/addressSpec.js
+++ test/models/addressSpec.js
@@ -1,16 +1,20 @@
 define(['js/models/address', 'test/fixtures/address'], function(AddressModel, AddressFixture){
 
-  describe('A model', function(){
+  describe('An address model', function(){
     it('should have certain defaults', function(){
       var address = new AddressModel();
       expect(address.get("firstName")).toBe("Jane");
     })
 
-    it('should take fixture json', function(){
+    it('should require a first name', function(){
       var address = new AddressModel(AddressFixture);
-      expect(address.get('firstName')).toBe(AddressFixture.firstName);
-      expect(address.get('lastName')).toBe(AddressFixture.lastName);
-      expect(address.get('twitter')).toBe(AddressFixture.twitter);
+
+      address.unset('firstName');
+      expect(address.isValid()).toBe(false);
+
+      address.set('firstName', AddressFixture.firstName);
+      expect(address.isValid()).toBe(true);
+
     });
   });
 
