f54f3758928e5e7033f3c750a6f601b2d767a2a2
Jonathan Freeman <freethejazz@gmail.com>
2014-03-06 17:13:15 -0600

    Switching from handlebars to require-handlebars-plugin

--- test/test-main.js
+++ test/test-main.js
@@ -15,7 +15,7 @@ requirejs.config({
     'jquery': 'bower_components/jquery/dist/jquery',
     'underscore': 'bower_components/underscore/underscore',
     'backbone': 'bower_components/backbone/backbone',
-    'handlebars': 'bower_components/handlebars/handlebars.amd'
+    'hbs': 'bower_components/require-handlebars-plugin/hbs'
   },
 
   shim: {
