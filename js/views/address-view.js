cfa602469c55302dc1a3cfb91871ed85a076cb24
Jonathan Freeman <freethejazz@gmail.com>
2014-03-06 17:56:22 -0600

    Listen to model to update view

--- js/views/address-view.js
+++ js/views/address-view.js
@@ -5,6 +5,9 @@ define([
 ], function(Backbone, $, addressTmpl){
   var AddressView = Backbone.View.extend({
     template: addressTmpl,
+    initialize: function(){
+      this.listenTo(this.model, 'change', this.render);
+    },
     render: function(){
       var modelJSON = this.model.toJSON();
       this.$el.html(this.template(modelJSON));
