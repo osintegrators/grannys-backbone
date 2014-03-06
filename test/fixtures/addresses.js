4814845ff4064e871f834c6a4393e17387617ee7
Jonathan Freeman <freethejazz@gmail.com>
2014-03-06 14:09:45 -0600

    Adding fixture for testing the collection

---
+++ test/fixtures/addresses.js
@@ -0,0 +1,21 @@
+define({
+  GET: {
+    addresses: [
+      {
+        firstName: 'Joe',
+        lastName: 'Blow',
+        twitter: '@bookofjoe'
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        twitter: '@planejane'
+      },
+      {
+        firstName: 'Jonathan',
+        lastName: 'Freeman',
+        twitter: '@freethejazz'
+      }
+    ]
+  }
+});
