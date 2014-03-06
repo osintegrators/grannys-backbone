a94604477682df4b43eedb856e0f18da0e0d37f2
Jonathan Freeman <freethejazz@gmail.com>
2014-03-06 17:55:26 -0600

    Adding address-view tests

--- test/views/addressSpec.js
+++ test/views/addressSpec.js
@@ -5,7 +5,7 @@ define([
 ],function(AddressView, AddressModel, ModelFixtures){
   describe('An address view', function(){
 
-    it('should render first name, last name, and twitter handle', function(){
+    xit('should render first name, last name, and twitter handle', function(){
       var address = new AddressModel(ModelFixtures);
       var addressView = new AddressView({ model: address});
       addressView.render();
@@ -14,5 +14,17 @@ define([
       expect(addressView.$('.last-name').text()).toBe(ModelFixtures.lastName);
       expect(addressView.$('.twitter').text()).toBe(ModelFixtures.twitter);
     });
+
+    it('should render when the model changes', function(){
+      var address = new AddressModel(ModelFixtures);
+      var addressView = new AddressView({ model: address});
+
+      address.set('firstName', 'foo');
+
+      expect(addressView.$('.first-name').text()).toBe('foo');
+      expect(addressView.$('.last-name').text()).toBe(ModelFixtures.lastName);
+      expect(addressView.$('.twitter').text()).toBe(ModelFixtures.twitter);
+    });
+
   });
 });
