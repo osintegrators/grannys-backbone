aab3516bbd20c1dba27a27a810af76ea1ecaf8b1
Jonathan Freeman <freethejazz@gmail.com>
2014-03-06 17:13:58 -0600

    Adding address view and template

---
+++ js/views/address-view.js
@@ -0,0 +1,17 @@
+define([
+  'backbone',
+  'jquery',
+  'hbs!templates/address'
+], function(Backbone, $, addressTmpl){
+  var AddressView = Backbone.View.extend({
+    template: addressTmpl,
+    render: function(){
+      var modelJSON = this.model.toJSON();
+      this.$el.html(this.template(modelJSON));
+    
+      return this;
+    }
+  });
+
+  return AddressView;
+});
