30068f9f717a93a99975e18238c286b94887b7bf
Jonathan Freeman <freethejazz@gmail.com>
2014-03-06 16:14:15 -0600

    Adding fake server in the collection test

--- test/collections/addressesSpec.js
+++ test/collections/addressesSpec.js
@@ -1,7 +1,8 @@
 define([
     'js/collections/addresses',
-    'js/models/address'
-    ], function(AddressCollection, AddressModel){
+    'js/models/address',
+    'test/fixtures/addresses'
+    ], function(AddressCollection, AddressModel, AddressesFixtures){
   
   describe('The address collection', function(){
   
@@ -20,9 +21,22 @@ define([
     });
 
     it('should be able to process a successful response from the server', function(){
+      // set up the fake server
+      var fakeServer = sinon.fakeServer.create();
+      fakeServer.respondWith('GET',
+        '/addresses',
+        [ 200,
+        { 'Content-type': 'application/json' },
+        JSON.stringify(AddressesFixtures.GET.addresses)
+        ]);
+
       var addresses = new AddressCollection();
       addresses.fetch();
+      fakeServer.respond();
       expect(addresses.length).toBe(3);
+
+      // tear down the fake server
+      fakeServer.restore();
     });
 
   });
